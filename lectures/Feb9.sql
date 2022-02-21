9dac2d631b2d59b158a015360efb5cde54d1e960
ChristianAyala <cayala@ordermygear.com>
2022-02-21 13:05:15 -0600

    Added updated Feb 9 sql

--- lectures/Feb9.sql
+++ lectures/Feb9.sql
@@ -30,7 +30,8 @@ CREATE TABLE customer (
 
 CREATE TABLE service_department (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30),
+    location_id INTEGER REFERENCES location(id)
 );
 
 CREATE TABLE employee (
@@ -67,3 +68,79 @@ CREATE TABLE employee_maintenance(
     maintenance_history_id INTEGER REFERENCES maintenance_history(id),
     PRIMARY KEY (employee_id, maintenance_history_id)
 );
+
+ALTER TABLE service_department
+ADD COLUMN location_id INTEGER REFERENCES location(id);
+
+
+-- What vehicles are in my system?
+SELECT * FROM vehicle;
+
+-- What vehicles are in my system? Include the name of the location they are currently
+-- parked at. If they are not currently assigned to a location (it is currently rented), I still
+-- want to see it in my result set.
+SELECT * FROM vehicle
+LEFT OUTER JOIN location on vehicle.location_id = location.id;
+
+-- What vehicles belong to Location cew (whatever you may have called it)?
+SELECT * FROM location;
+SELECT * FROM location
+JOIN vehicle v on location.id = v.location_id
+WHERE location.id = 50970;
+
+SELECT * FROM vehicle
+JOIN location l on vehicle.location_id = l.id
+WHERE l.name = 'Location cew';
+
+SELECT * FROM vehicle
+WHERE vehicle.location_id = 50970;
+
+-- Give me the reservation history for Vehicle 1. This should include the vehicle info and the
+-- customer info, but not the transaction info.
+SELECT * FROM reservation;
+SELECT * FROM reservation
+JOIN vehicle v on reservation.vehicle_id = v.vin
+WHERE vin = 'wtwiTLTBn)P&38x';
+
+SELECT v.*, reservation.*, c.first_name FROM reservation
+JOIN vehicle v ON reservation.vehicle_id = v.vin
+JOIN customer c ON reservation.customer_id = c.id
+WHERE vin = 'wtwiTLTBn)P&38x';
+
+-- Provide the transactions for Reservation 1.
+SELECT * FROM reservation;
+SELECT * FROM transaction
+JOIN reservation r on transaction.reservation_id = r.id
+WHERE reservation_id = 51409;
+SELECT * FROM transaction
+WHERE reservation_id = 51409;
+SELECT * FROM transaction;
+
+SELECT SUM(amount) FROM transaction
+WHERE reservation_id = 51409;
+
+-- Provide the net amount of money that has been tracked in the system
+SELECT SUM(amount) FROM transaction;
+
+-- Provide the maintenance history for Vehicle 1
+SELECT * FROM vehicle;
+SELECT * FROM maintenance_history
+WHERE vehicle_id = '3k$e0je!yyZqiC@';
+SELECT * FROM maintenance_history;
+
+-- Provide the names of all the employees who have provided service on Vehicle 1.
+SELECT * FROM employee_maintenance
+JOIN employee e on employee_maintenance.employee_id = e.id
+JOIN maintenance_history mh on employee_maintenance.maintenance_history_id = mh.id
+WHERE vehicle_id = '3k$e0je!yyZqiC@';
+
+
+TRUNCATE TABLE employee_maintenance CASCADE ;
+TRUNCATE TABLE maintenance_history CASCADE;
+TRUNCATE TABLE transaction;
+TRUNCATE TABLE reservation CASCADE ;
+TRUNCATE TABLE employee CASCADE ;
+TRUNCATE TABLE service_department CASCADE ;
+TRUNCATE TABLE customer CASCADE ;
+TRUNCATE TABLE vehicle CASCADE ;
+TRUNCATE TABLE location CASCADE ;
